d952d27c6543a1fec2fdf7150c6a5160d839d9ea
Mesup Adhikari <mesupadhikari@gmail.com>
2024-11-21 16:02:49 +0545

    Update page.jsx

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -3,6 +3,6 @@ import { SignIn } from '@clerk/nextjs'
 export default function Page() {
   return (
   <div className="flex items-center justify-center h-screen">
-  <SignIn />
+  <SignIn redirectUrl="/dashboard" />
   </div>)
 }
